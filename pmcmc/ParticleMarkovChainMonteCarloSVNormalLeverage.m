86542f8054fd6789afa5d9f1697aded1eebc0063
Philippe Rémy <philippe.remy@junior-aei.com>
2015-05-19 16:31:46 +0100

    Normal leverage 2
    Normal leverage 2

--- pmcmc/ParticleMarkovChainMonteCarloSVNormalLeverage.m
+++ pmcmc/ParticleMarkovChainMonteCarloSVNormalLeverage.m
@@ -18,12 +18,13 @@ classdef ParticleMarkovChainMonteCarloSVNormalLeverage < ParticleMarkovChainMont
  
         function [log_val] = call_likelihood(this, hmm, fieldname, fieldval, step)
             switch fieldname
+                %(y, rho_prop, sigma_prop, beta, N, cor)
                 case 'rho_prop'
                     log_val = Compute_Log_Likelihood_NormalLeverage(hmm.y, fieldval, this.sigma_prop(step-1), hmm.beta, this.particles, this.cor_prop(step-1));
                 case 'sigma_prop'
                     log_val = Compute_Log_Likelihood_NormalLeverage(hmm.y, this.rho_prop(step-1), fieldval, hmm.beta, this.particles, this.cor_prop(step-1));
                 case 'cor_prop'
-                    log_val = Compute_Log_Likelihood_NormalLeverage(hmm.y, this.rho_prop(step-1), fieldval, hmm.beta, this.particles, this.cor_prop(step-1));
+                    log_val = Compute_Log_Likelihood_NormalLeverage(hmm.y, this.rho_prop(step-1), this.sigma_prop(step-1), hmm.beta, this.particles, fieldval);
             end
         end
         
@@ -39,8 +40,8 @@ classdef ParticleMarkovChainMonteCarloSVNormalLeverage < ParticleMarkovChainMont
                     val = 1/gamrnd(1, 1/1); %inverse gamma
                 case 'cor_prop'
                     val = -1 + 2*rand; %unif
+                    %val = 0.9*(-1 + 2*rand); %unif(-0.9, 0.9)
             end
-        end
-        
+        end 
     end
 end
