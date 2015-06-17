0b8312f0542b22d167819704b08d60ac31056f19
Philippe Rémy <philippe.remy@ullink.com>
2015-06-17 11:22:21 +0200

    Associated Likelihood modules (ESS and no ESS)
    Associated Likelihood modules (ESS and no ESS)

--- likelihoods/Ex4_Compute_Log_Likelihood.m
+++ likelihoods/Ex4_Compute_Log_Likelihood.m
@@ -1,7 +1,7 @@
 
-function [Y_log_SV] = Ex4_Compute_Log_Likelihood(y, rho_prop, sigma_prop, beta, N, nu)
+function [Y_log_SV, estX] = Ex4_Compute_Log_Likelihood(y, rho_prop, sigma_prop, beta, N, nu)
     steps = size(y, 2);
-    [~, ~, p_y] = Ex2_bootstrap_filter_SIR(y, rho_prop, sigma_prop, beta, N, steps, nu);
+    [estX, ~, p_y] = Ex2_bootstrap_filter_SIR(y, rho_prop, sigma_prop, beta, N, steps, nu);
     Y_log_SV = Ex2_Estimate_Py( p_y , steps );
     Y_log_SV = Y_log_SV(length(Y_log_SV));
 end
