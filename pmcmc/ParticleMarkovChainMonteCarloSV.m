d9e2013dfd801d1d9a99f31632cdd9d8b99986b1
Philippe Rémy <philippe.remy@ullink.com>
2015-06-18 18:42:53 +0200

    PMCMC Std SV

--- pmcmc/ParticleMarkovChainMonteCarloSV.m
+++ pmcmc/ParticleMarkovChainMonteCarloSV.m
@@ -4,8 +4,9 @@ classdef ParticleMarkovChainMonteCarloSV < ParticleMarkovChainMonteCarlo
 	
         rho_prop = {};
         sigma_prop = {};
-        nu_prop = {};
+        beta_prop = {};
 
+        p_y_given_x = @(y,sig) normpdf(y, 0, sig);
     end
   
     methods
@@ -13,35 +14,40 @@ classdef ParticleMarkovChainMonteCarloSV < ParticleMarkovChainMonteCarlo
             this = this@ParticleMarkovChainMonteCarlo(steps_mcmc, particles);
             this.rho_prop = zeros(1, this.steps_mcmc);
             this.sigma_prop = zeros(1, this.steps_mcmc);
-            this.nu_prop = zeros(1, this.steps_mcmc);
+            this.beta_prop = zeros(1, this.steps_mcmc);
         end
  
         function [log_val] = call_likelihood(this, hmm, fieldname, fieldval, step)
             switch fieldname
                 case 'rho_prop'
-                    log_val = Ex4_Compute_Log_Likelihood(hmm.y, fieldval, this.sigma_prop(step-1), hmm.beta, this.particles, this.nu_prop(step-1));
+                    log_val = BootstrapParticleFilter(hmm.y, fieldval, this.sigma_prop(step-1), this.beta_prop(step-1), this.particles, this.p_y_given_x);
                 case 'sigma_prop'
-                    log_val = Ex4_Compute_Log_Likelihood(hmm.y, this.rho_prop(step-1), fieldval, hmm.beta, this.particles, this.nu_prop(step-1));
-                case 'nu_prop'
-                    log_val = Ex4_Compute_Log_Likelihood(hmm.y, this.rho_prop(step-1), this.sigma_prop(step-1), hmm.beta, this.particles, fieldval);
+                    log_val = BootstrapParticleFilter(hmm.y, this.rho_prop(step-1), fieldval, this.beta_prop(step-1), this.particles, this.p_y_given_x);
+                case 'beta_prop'
+                    log_val = BootstrapParticleFilter(hmm.y, this.rho_prop(step-1), this.sigma_prop(step-1), fieldval, this.particles, this.p_y_given_x);
             end
         end
         
         function [log_val] = call_likelihood_denom(this, hmm, step)
-           log_val = Ex4_Compute_Log_Likelihood(hmm.y, this.rho_prop(step-1), this.sigma_prop(step-1), hmm.beta, this.particles, this.nu_prop(step-1)); 
+           log_val = BootstrapParticleFilter(hmm.y, this.rho_prop(step-1), this.sigma_prop(step-1), this.beta_prop(step-1), this.particles, this.p_y_given_x); 
         end
         
         
-        function [val] = call_prior(this, fieldname)
+        function [val] = call_prior(~, fieldname)
             switch fieldname
                 case 'rho_prop'
                     val = -1 + 2*rand; %unif
                 case 'sigma_prop'
                     val = 1/gamrnd(1, 1/1); %inverse gamma
-                case 'nu_prop'
+                    if(val > 5)
+                       val = 5; 
+                    end
+                case 'beta_prop'
                     val = 1/gamrnd(1, 1/1); %inverse gamma
+                    if(val > 5)
+                       val = 5; 
+                    end
             end
         end
-        
     end
 end
