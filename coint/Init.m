72490a79e84bf1e7fd14e2e8a4f3f924fb16668b
Philippe Rémy <philippe.remy@ullink.com>
2015-07-15 09:47:02 +0200

    Init move to coint

---
+++ coint/Init.m
@@ -0,0 +1,5 @@
+clear;
+addpath('impl/');
+load ../data/spx.mat; 
+format shortG;
+spx = csvread('../data/spx.csv');
--- coint/old/Init.m
+++
@@ -1,5 +0,0 @@
-clear;
-addpath('impl/');
-load ../spx.mat; 
-format shortG;
-spx = csvread('../spx.csv');
