01ea6e781573d2dc107fe8f9912a9d6221d3f225
Philippe Rémy <philippe.remy@junior-aei.com>
2015-08-10 11:56:20 +0200

    Thesis update

---
+++ scripts/Draw_Bollinger_Bands.m
@@ -0,0 +1,16 @@
+function [ ] = Draw_Bollinger_Bands( range, wsize, nstd, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, hasLegend )
+    figure;
+    [mid, uppr, lowr] = bollinger(dis_CLOSE(range), wsize, 0, nstd);
+    plot([Fit_NA(mid) Fit_NA(uppr) Fit_NA(lowr)]);
+    hold on;
+    candle(dis_HIGH(range), dis_LOW(range), dis_CLOSE(range), dis_OPEN(range), 'black');
+    if(hasLegend)
+        legend('mid band', 'upper band', 'lower band', 'stock price');
+    end
+    hold on;
+    xlim([wsize range(end)-range(1)]);
+    ylabel('Prices (USD)');
+    xlabel('Time (Days)');
+
+end
+
--- scripts/PIC_Bollinger_bands.m
+++ scripts/PIC_Bollinger_bands.m
@@ -1,19 +1,7 @@
 load disney;
-candle(dis_HIGH(end-20:end), dis_LOW(end-20:end), dis_CLOSE(end-20:end),...
-dis_OPEN(end-20:end), 'b');
+addpath('../helpers');
 
-range = 400:600;
-figure;
-[mid, uppr, lowr] = bollinger(dis_CLOSE(range));
-plot([Fit_NA(mid) Fit_NA(uppr) Fit_NA(lowr)]);
-hold on;
-candle(dis_HIGH(range), dis_LOW(range), dis_CLOSE(range), dis_OPEN(range), 'black');
-legend('mid band', 'upper band', 'lower band', 'stock price');
-hold on;
-xlim([20 200]);
-ylabel('Prices (USD)');
-xlabel('Time (Days)');
-
-
-
-a = GetPriceArray(pp, [236 237 238 239]);
+Draw_Bollinger_Bands( 400:600, 20, 2, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
+Draw_Bollinger_Bands( 400:600, 20, 1, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
+Draw_Bollinger_Bands( 400:600, 50, 1, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
+Draw_Bollinger_Bands( 400:600, 50, 2, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
