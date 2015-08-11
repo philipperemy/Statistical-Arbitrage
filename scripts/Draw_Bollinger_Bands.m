3b1e4142e501a925ae8e6957c961f88693a935c4
Philippe Rémy <philippe.remy@junior-aei.com>
2015-08-12 00:30:08 +0200

    Thesis update

--- scripts/Draw_Bollinger_Bands.m
+++ scripts/Draw_Bollinger_Bands.m
@@ -1,7 +1,7 @@
 function [ ] = Draw_Bollinger_Bands( range, wsize, nstd, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, hasLegend )
     figure;
     [mid, uppr, lowr] = bollinger(dis_CLOSE(range), wsize, 0, nstd);
-    plot([Fit_NA(mid) Fit_NA(uppr) Fit_NA(lowr)]);
+    plot([Fit_NA(mid) Fit_NA(uppr) Fit_NA(lowr)], 'LineWidth', 2);
     hold on;
     candle(dis_HIGH(range), dis_LOW(range), dis_CLOSE(range), dis_OPEN(range), 'black');
     if(hasLegend)
--- scripts/PIC_Bollinger_bands.m
+++ scripts/PIC_Bollinger_bands.m
@@ -1,7 +1,7 @@
 load disney;
 addpath('../helpers');
 
-Draw_Bollinger_Bands( 400:600, 20, 2, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
 Draw_Bollinger_Bands( 400:600, 20, 1, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
-Draw_Bollinger_Bands( 400:600, 50, 1, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
-Draw_Bollinger_Bands( 400:600, 50, 2, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
+Draw_Bollinger_Bands( 400:600, 20, 2, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
+Draw_Bollinger_Bands( 400:600, 40, 1.5, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
+Draw_Bollinger_Bands( 400:600, 40, 2.5, dis_HIGH, dis_LOW, dis_CLOSE, dis_OPEN, false );
