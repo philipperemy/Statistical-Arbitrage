09c5eea86730bad7267f1a4e69920a1a5a9d5c75
Philippe Rémy <philippe.remy@junior-aei.com>
2015-08-22 22:56:42 +0200

    Minor update

--- strategy/strategy-running/Plot_Strats.m
+++ strategy/strategy-running/Plot_Strats.m
@@ -2,12 +2,15 @@
 %portfolio_cumsum_b
 %portfolio_cumsum_z
 
-T = length(portfolio_cumsum_b);
-disp_seq = 1:T-1;
-%FALSE FIGURE THIS OUT
-spx_cumsum = ((10000/spx(166))*spx(166:end));
+LEN = length(portfolio_cumsum_b);
+disp_seq = 1:LEN-1;
+T = 169;
+spx_cumsum = ((10000/spx(T))*spx(T:end));
 spx_cumsum = spx_cumsum(disp_seq);
 
 Y = [portfolio_cumsum_b(disp_seq)' portfolio_cumsum_z(disp_seq)' spx_cumsum];
 plot(Y);
 grid on;
+
+xlabel('Time (days)');
+ylabel('Valuation (USD)');
