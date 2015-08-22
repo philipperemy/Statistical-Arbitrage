97aa43c60e9161b780458e2efb83b3d61dc3c1fc
Philippe Rémy <philippe.remy@ullink.com>
2015-08-22 17:55:46 +0200

    Bug fixed on Bollinger Bands with SV

--- strategy/Compute_Bollinger_Bands_SV.m
+++ strategy/Compute_Bollinger_Bands_SV.m
@@ -5,12 +5,12 @@ function [ mid, uppr, lowr ] = Compute_Bollinger_Bands_SV(data, wsize, wts, nstd
     %GENERATED PROCESSES CAN BE DONE BEFOREHAND
     
     returns_volatility_sd = sqrt(returns_volatility_var);
-    M = 100;%for speed. MC param
+    M = 1000;%for speed. MC param
     T = length(data);
     generated_processes = zeros(M, T);
     vol_returns = zeros(T,1);
     for t = 2:T
-        vol_returns(t) = returns_volatility_sd(t)*data(t-1);
+        vol_returns(t) = returns_volatility_sd(t)*abs(data(t-1));
         generated_processes(:,t) = normrnd(data(t-1), vol_returns(t), M, 1);
     end
     
