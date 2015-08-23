aa23f4c3c593078fb420f69c5e6a836b0f47d54f
Philippe Rémy <philippe.remy@junior-aei.com>
2015-08-24 01:52:43 +0200

    Thesis update

--- helpers/CleanName.m
+++ helpers/CleanName.m
@@ -2,5 +2,6 @@ function [ str ] = CleanName( str )
     str = strgsub(char(str), 'UN Equity', '');
     str = strgsub(str, 'UQ Equity', '');
     str = strgsub(str, 'UW Equity', '');
+    str = strgsub(str, 'US Equity', '');
 end
 
--- strategy/strategy-running/Run_Bollinger_Complex_Strategy.m
+++ strategy/strategy-running/Run_Bollinger_Complex_Strategy.m
@@ -70,3 +70,32 @@ portfolio_cumsum = portfolio_cumsum / length(spreads_ids);
 PerformanceAssessment(portfolio_cumsum, spx(T:end), initial_bet)
 
 portfolio_cumsum_sv = portfolio_cumsum;
+
+
+%double dataset not that goood.
+tuples = [];
+for i = spreads_ids
+   spread = spreads(i);
+   
+   c = find(mat_t(:,5)==i);
+   sro = mat_t(c,3);
+   tr = mat_t(c,4);
+   mdd = mat_t(c,6);
+   net = mat_t(c,1);
+
+   sri = mat_a(i,3);
+   s = sprintf('%d, %s & %s & %s & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %.2f & %i \\\\', i, char(CleanName(pp.tickers(spread.tuple(1)))), char(CleanName(pp.tickers(spread.tuple(2)))), char(CleanName(pp.tickers(spread.tuple(3)))),...
+       spread.beta(1), spread.beta(2), spread.beta(3), sri, sro, net, mdd, tr);
+   tuples(end+1) = spread.tuple(1);
+   tuples(end+1) = spread.tuple(2);
+   tuples(end+1) = spread.tuple(3);
+   disp(s);
+end
+
+tuples = unique(tuples);
+for i = 1:length(pp.tickers)
+   if(sum(find(i==tuples)~=0))
+       s = sprintf('%s & %s & %s \\\\', char(CleanName(pp.tickers(i))), char(pp.names(i)), char(pp.sector(i)));
+       disp(s);
+   end
+end
